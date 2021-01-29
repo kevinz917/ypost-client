5e65ce83fba37ed08d435f7cee8dc31c3a32e049
Kevin Z <zkw917@gmail.com>
2021-01-29 22:59:30 +0800

    Base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
+import { Base } from "../util/base";
 
 const cookies = new Cookies();
 const api = axios.create();
 
 // base url
-if (process.env.NODE_ENV === "production") {
-  api.defaults.baseURL = "";
-} else {
-  api.defaults.baseURL = "http://localhost:5000/";
-}
+api.defaults.baseURL = Base;
 
 api.interceptors.request.use(async (config) => {
   config.headers = { "access-token": cookies.get("ypostUser").accessToken };
